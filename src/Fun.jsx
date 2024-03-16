34380e1a683293274ebdc8f4f4e52a906de34994
Kaizen <125158784+kaizen011@users.noreply.github.com>
2024-03-17 00:04:35 +0100

    Following Odin-Project docs

--- src/Fun.jsx
+++ src/Fun.jsx
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid';
+
 function Fun() {
     let numbers = [1,2,3,4,5,6,7,8,9,10]
     return (
@@ -5,7 +7,7 @@ function Fun() {
       <ul>
         { numbers.map((evenNumbers)=> (
             
-               evenNumbers % 2 === 0 ? (<li key={evenNumbers}>{evenNumbers}</li>) : null
+               evenNumbers % 2 === 0 && (<li key={evenNumbers}>{evenNumbers}</li>) 
              
         ))} 
       </ul>
